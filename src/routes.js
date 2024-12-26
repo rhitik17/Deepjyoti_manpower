5548b7a85200db73ce69afc1e232f5d00ae46d62
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-12-26 16:26:38 +0545

    buid success

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Home from "./views/pages/Home";
 import Jobs from "./views/pages/Jobs";
 import RecruitmentProcess from "./views/pages/RecruitmentProcess";
 import Services from "./views/pages/Services";
-import WeProvide from "./views/pages/WeProvide";
+
 
 const routeObjs = {
 
@@ -18,7 +18,7 @@ const routeObjs = {
     { path: "/about-us", component: AboutUs },
     { path: "/contact-us", component: ContactUs },
     { path: "/services", component: Services },
-    { path: "/we-provide", component: WeProvide },
+
     { path: "/recruitment-process", component: RecruitmentProcess },
     { path: "/advertisements", component: Advertisement },
 
