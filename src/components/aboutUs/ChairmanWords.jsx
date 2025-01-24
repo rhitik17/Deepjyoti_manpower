cdd4b73aeae3beb7e6082dbb4785a58a8fbc711c
Rhitik_HBR <ritikbhandari17@gmail.com>
2025-01-24 23:01:48 +0545

    feat: add phone to carousel

--- src/components/aboutUs/ChairmanWords.jsx
+++ src/components/aboutUs/ChairmanWords.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 const ChairmanWords = ({ data }) => {
-  console.log(data);
   return (
-    <div className="w-full h-auto flex flex-col md:flex-row justify-center md:justify-start items-center max-md:space-y-10 gap-4 lg:gap-10 py-8 xl:py-16">
-      <div className="md:w-3/12 lg:w-4/12  md:h-[350px] flex items-center justify-center">
+    <div className="w-full h-auto self-center flex flex-col lg:flex-row justify-center md:justify-start items-center max-md:space-y-10 gap-4 lg:gap-10 py-8 xl:py-16">
+      <div className="w-full  md:w-3/12 lg:w-4/12  md:h-[350px] flex items-center justify-center">
         {/* Full-height image */}
         <img
           className="rounded-lg  w-full md:w-[300px] object-fit"
@@ -13,7 +12,7 @@ const ChairmanWords = ({ data }) => {
         />
       </div>
 
-      <div className="md:w-9/12 lg:w-8/12 flex flex-col justify-start items-start gap-8">
+      <div className="w-full md:w-9/12 lg:w-8/12 flex flex-col justify-start items-start gap-8">
         <div className="flex flex-col w-full justify-start items-center gap-12">
           <div className="text-white text-md lg:text-xl font-semibold">
             <p className="pb-2 ">{data?.greeting}</p>
@@ -25,12 +24,45 @@ const ChairmanWords = ({ data }) => {
           </div>
         </div>
         <div className="w-full h-0.5 bg-white"></div>
-        <div className="flex flex-col justify-start gap-1">
-          <div className="text-primary-yellow text-2xl font-semibold ">
-            {data?.name}
+
+        <div className="w-full  flex flex-col lg:flex-row justify-between gap-3 lg:items-center">
+          <div className=" flex flex-col justify-start gap-1">
+            <div className="text-primary-yellow text-2xl font-semibold ">
+              {data?.name}
+            </div>
+            <div className="text-white text-lg font-semibold font-['Inter']">
+              {data?.title}
+            </div>
           </div>
-          <div className="text-white text-lg font-semibold font-['Inter']">
-            {data?.title}
+
+          {/* phone */}
+          <div className=" flex flex-col gap-1">
+            {data?.phone.WhatsApp && (
+              <span className=" flex gap-2 font-semibold lg:text-lg text-white">
+                WhatsApp:{" "}
+                <h3 className="  font-semibold text-primary-yellow flex-wrap">
+                  {data?.phone.WhatsApp}
+                </h3>
+              </span>
+            )}
+
+            {data?.phone.Botim && (
+              <span className=" flex gap-2 font-semibold lg:text-lg text-white">
+                Botim:
+                <h3 className="  font-semibold text-primary-yellow">
+                  {data?.phone.Botim}
+                </h3>
+              </span>
+            )}
+
+            {data?.phone.IMO && (
+              <span className=" flex gap-2 font-semibold lg:text-lg text-white">
+                IMO:
+                <h3 className="  font-semibold text-primary-yellow">
+                  {data?.phone.IMO}
+                </h3>
+              </span>
+            )}
           </div>
         </div>
       </div>
--- src/data/carouseldata.js
+++ src/data/carouseldata.js
@@ -1,39 +1,71 @@
-export const  carouselData = [
-    {
-      id: 1,
-      image: "/images/chairman1.svg",
-      greeting: "Greetings from Deepjyoti Manpower Service,",
-      quote: [
-        "We are a government-authorized manpower supply company based in Kathmandu, Nepal (License No. 1136/073/074). We provide skilled, semi-skilled, and unskilled workers for various international demands.",
-        "Our employees are diligent, hardworking, disciplined, and cooperative. We focus on preparing them to meet organizational needs.",
-        "With a strong track record of quality service, we serve satisfied clients globally by supplying a highly trained workforce. We aim to build partnerships for shared objectives and deliver the best manpower services Nepal can offer."
-      ],
-      name: "Shiv Nath Sah",
-      title: "Chairman"
-    },
-    
-      {
-        id: 2,
-        image: "/images/md.svg", 
-        greeting: "Greetings from Deepjyoti Manpower Service,",
-        quote: [
-          "First of all, we would like to extend sincere thanks for your valuable interest in our organization. Please go through our company profile and website for detailed information, procedure, quotation, quality, and reliability of our sources of manpower.",
-          "We hope that it would be a useful direction for you to recruit our good, honest, dutiful, obedient, disciplined, and strong workers for your company.",
-          "Please feel free to contact us at any time for further information regarding the recruitment. We always regard our clients as our most important valuables. We look forward to receiving your positive response. We assure you that our best service and cooperation will be for you at all times."
-        ],
-        name: "Krishna kumar Shah",
-        title: "Marketing Director / Int'l Marketing Director"
-      },
-      {
-        id: 3,
-        image: "/images/imd.svg", 
-        greeting: "Dear Sir / Madam,",
-        quote: [
-          "I heartily take it as a delight in greeting and welcoming you to Deepjyoti Manpower Service Pvt. Ltd., which will take charge of all your requirements for tentative manpower service. Deepjyoti Manpower Service Pvt. Ltd. has been working as a bridge between foreign industries and Nepalese local manpower.",
-          "We have the best experience in generating and developing the feeling of transparency, efficiency, promptness, and good governance with foreign business industries. We provide the best services to the valued job holders at all times to make the business fruitful for our mutual benefits.",
-          "Once again, I would like to thank you for selecting Deepjyoti Manpower Service Pvt. Ltd. as your business partner and ensure you a safe and durable business with us."
-        ],
-        name: "Rakesh Shah",
-        title: "Int'l Marketing Director"
-      }
-    ];
+
+export const carouselData = [
+  {
+    id: 1,
+    image: "/images/chairman1.svg",
+    greeting: "Greetings from Deepjyoti Manpower Service,",
+    quote: [
+      "We are a government-authorized manpower supply company based in Kathmandu, Nepal (License No. 1136/073/074). We provide skilled, semi-skilled, and unskilled workers for various international demands.",
+      "Our employees are diligent, hardworking, disciplined, and cooperative. We focus on preparing them to meet organizational needs.",
+      "With a strong track record of quality service, we serve satisfied clients globally by supplying a highly trained workforce. We aim to build partnerships for shared objectives and deliver the best manpower services Nepal can offer."
+    ],
+    name: "Shiv Nath Sah",
+    title: "Chairman",
+    phone: {
+      WhatsApp: "+977-9801671085",
+      Botim: "+977-9823370076",
+      IMO: "+977-9823716030"
+    }
+  },
+  {
+    id: 2,
+    image: "/images/md.svg",
+    greeting: "Greetings from Deepjyoti Manpower Service,",
+    quote: [
+      "First of all, we would like to extend sincere thanks for your valuable interest in our organization. Please go through our company profile and website for detailed information, procedure, quotation, quality, and reliability of our sources of manpower.",
+      "We hope that it would be a useful direction for you to recruit our good, honest, dutiful, obedient, disciplined, and strong workers for your company.",
+      "Please feel free to contact us at any time for further information regarding the recruitment. We always regard our clients as our most important valuables. We look forward to receiving your positive response. We assure you that our best service and cooperation will be for you at all times."
+    ],
+    name: "Krishna Kumar Shah",
+    title: "Marketing Director / Int'l Marketing Director",
+    phone: {
+      WhatsApp: "+977-9860207066",
+      Botim: "+977-9808248185",
+     
+    }
+  },
+  {
+    id: 3,
+    image: "/images/imd.svg",
+    greeting: "Dear Sir / Madam,",
+    quote: [
+      "I heartily take it as a delight in greeting and welcoming you to Deepjyoti Manpower Service Pvt. Ltd., which will take charge of all your requirements for tentative manpower service. Deepjyoti Manpower Service Pvt. Ltd. has been working as a bridge between foreign industries and Nepalese local manpower.",
+      "We have the best experience in generating and developing the feeling of transparency, efficiency, promptness, and good governance with foreign business industries. We provide the best services to the valued job holders at all times to make the business fruitful for our mutual benefits.",
+      "Once again, I would like to thank you for selecting Deepjyoti Manpower Service Pvt. Ltd. as your business partner and ensure you a safe and durable business with us."
+    ],
+    name: "Saim Mansur",
+    title: "Int'l Marketing Director",
+    phone: {
+      WhatsApp: "+977-9801101712",
+      Botim: "+977-9844323636",
+      IMO: "+977-9801101713"
+    }
+  },
+  {
+    id: 4,
+    image: "/images/accountant.jpg",
+    greeting: "Dear Sir / Madam,",
+    quote: [
+      "It is my privilege to welcome you to Deepjyoti Manpower Service Pvt. Ltd., where we are committed to meeting all your manpower needs with precision and reliability. Our company has built a reputation as a trusted bridge between international industries and Nepalese manpower.",
+      "We pride ourselves on delivering services that embody transparency, efficiency, and responsiveness, ensuring mutual benefits and long-term partnerships.",
+      "Thank you for choosing us as your manpower service provider. We guarantee a seamless and fruitful collaboration, backed by our commitment to excellence and professionalism."
+    ],
+    name: "Janardan Shah",
+    title: "Accountant",
+    phone: {
+      WhatsApp: "",
+      Botim: "",
+      IMO: ""
+    }
+  }
+];
